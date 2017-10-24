0a2ff7c330d898bc1030dad7acd5111e5fdfd9fa
ArturBmstu <artur050711@gmail.com>
2017-10-24 22:27:04 +0300

    EnableTransactionManagement

--- src/main/java/ru/mail/park/AppConfiguration.java
+++ src/main/java/ru/mail/park/AppConfiguration.java
@@ -5,10 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import ru.mail.park.controllers.domain.User;
 import ru.mail.park.dto.UserDTO;
 
 @Configuration
+@EnableTransactionManagement
 public class AppConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
