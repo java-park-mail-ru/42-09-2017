4a4fe65b982ede31cc4480cafc60fe2fb31f4d62
CoolUser <fourtytwo@ubuntu.ruvds.com>
2017-11-16 15:57:03 +0300

    fix

--- src/main/java/ru/mail/park/websocket/SocketHandler.java
+++ src/main/java/ru/mail/park/websocket/SocketHandler.java
@@ -73,39 +73,40 @@ public class SocketHandler extends TextWebSocketHandler {
         }
         String textMessage = message.getPayload();
         ClientSnap snap;
-        WorldParser worldParser = gameDao.getLastParser();
+        //WorldParser worldParser = gameDao.getLastParser();
         handleMessage(Id.of(userId), message);
-        if (textMessage.equals("start")) {
-            Thread thread = new Thread(worldParser);
-            thread.start();
-            LOGGER.info("Simulation started");
-            session.sendMessage(new TextMessage("STARTED"));
-            return;
-        } else {
-            try {
-                snap = mapper.readValue(message.getPayload(), ClientSnap.class);
-                LOGGER.debug(message.getPayload());
-            } catch (IOException e) {
-                LOGGER.error("Wrong format");
-                session.sendMessage(new TextMessage("WrongFormat"));
-                return;
-            }
-        }
+	return;
+       // if (textMessage.equals("start")) {
+       //     Thread thread = new Thread(worldParser);
+       //     thread.start();
+       //     LOGGER.info("Simulation started");
+       //     session.sendMessage(new TextMessage("STARTED"));
+       //     return;
+       // } else {
+       //     try {
+       //         snap = mapper.readValue(message.getPayload(), ClientSnap.class);
+       //         LOGGER.debug(message.getPayload());
+       //     } catch (IOException e) {
+       //         LOGGER.error("Wrong format");
+       //         session.sendMessage(new TextMessage("WrongFormat"));
+       //         return;
+       //     }
+       // }
 
-        List<BodyFrame> bodyFrames = snap.getBodies();
-        LOGGER.info("Got changes");
-        for (BodyFrame bodyFrame : bodyFrames) {
-            Map<Long, BodyFrame> serverDiffs = worldParser.getDiffsPerFrame().get(bodyFrame.getId());
-            BodyFrame serverFrame = serverDiffs.get(snap.getFrame());
-            Vec2 serverPos = new Vec2(serverFrame.getPosition().x, -serverFrame.getPosition().y);
-            Vec2 serverVel = new Vec2(serverFrame.getVelocity().x, -serverFrame.getVelocity().y);
-            float serverAngle = -serverFrame.getAngle();
-            bodyFrame.setPosition(serverPos.sub(bodyFrame.getPosition()));
-            bodyFrame.setVelocity(serverVel.sub(bodyFrame.getVelocity()));
-            bodyFrame.setAngle(serverAngle - bodyFrame.getAngle());
-        }
-        session.sendMessage(new TextMessage(mapper.writeValueAsString(snap)));
-        LOGGER.info("Message is sent");
+       // List<BodyFrame> bodyFrames = snap.getBodies();
+      //  LOGGER.info("Got changes");
+      //  for (BodyFrame bodyFrame : bodyFrames) {
+        //    Map<Long, BodyFrame> serverDiffs = worldParser.getDiffsPerFrame().get(bodyFrame.getId());
+          //  BodyFrame serverFrame = serverDiffs.get(snap.getFrame());
+            //Vec2 serverPos = new Vec2(serverFrame.getPosition().x, -serverFrame.getPosition().y);
+           // Vec2 serverVel = new Vec2(serverFrame.getVelocity().x, -serverFrame.getVelocity().y);
+           // float serverAngle = -serverFrame.getAngle();
+           // bodyFrame.setPosition(serverPos.sub(bodyFrame.getPosition()));
+           // bodyFrame.setVelocity(serverVel.sub(bodyFrame.getVelocity()));
+           // bodyFrame.setAngle(serverAngle - bodyFrame.getAngle());
+       // }
+       // session.sendMessage(new TextMessage(mapper.writeValueAsString(snap)));
+        //LOGGER.info("Message is sent");
     }
 
     public void handleMessage(Id<User> userId, TextMessage textMessage) {
