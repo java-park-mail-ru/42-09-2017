946fb7f9c095013361b404442dfb383afc8a8a09
ArturBmstu <artur050711@gmail.com>
2017-11-24 01:06:54 +0400

    good fix

--- src/main/java/ru/mail/park/websocket/handlers/FinishHandler.java
+++ src/main/java/ru/mail/park/websocket/handlers/FinishHandler.java
@@ -5,12 +5,13 @@ import ru.mail.park.domain.Id;
 import ru.mail.park.domain.User;
 import ru.mail.park.mechanics.GameMechanics;
 import ru.mail.park.websocket.MessageHandlerContainer;
+import ru.mail.park.websocket.message.from.FinishMessage;
 import ru.mail.park.websocket.message.to.FinishedMessage;
 
 import javax.annotation.PostConstruct;
 
 @Service
-public class FinishHandler extends MessageHandler<FinishedMessage> {
+public class FinishHandler extends MessageHandler<FinishMessage> {
     private MessageHandlerContainer messageHandlerContainer;
     private GameMechanics gameMechanics;
 
@@ -18,18 +19,18 @@ public class FinishHandler extends MessageHandler<FinishedMessage> {
             MessageHandlerContainer messageHandlerContainer,
             GameMechanics gameMechanics
     ) {
-        super(FinishedMessage.class);
+        super(FinishMessage.class);
         this.messageHandlerContainer = messageHandlerContainer;
         this.gameMechanics = gameMechanics;
     }
 
     @PostConstruct
     public void init() {
-        messageHandlerContainer.registerHandler(FinishedMessage.class, this);
+        messageHandlerContainer.registerHandler(FinishMessage.class, this);
     }
 
     @Override
-    public void handle(FinishedMessage message, Id<User> userId) throws Exception {
+    public void handle(FinishMessage message, Id<User> userId) throws Exception {
         gameMechanics.handleFinish(userId);
     }
 }
