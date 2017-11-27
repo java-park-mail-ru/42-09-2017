df3d0659aa7a68297c31447af72af3d12dc0c44f
ArturBmstu <artur050711@gmail.com>
2017-11-27 18:29:10 +0400

    mechanics refactoring started

--- src/main/java/ru/mail/park/mechanics/MechanicsExecutor.java
+++ src/main/java/ru/mail/park/mechanics/MechanicsExecutor.java
@@ -1,34 +1,51 @@
-//package ru.mail.park.mechanics;
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.stereotype.Service;
-//
-//import javax.annotation.PostConstruct;
-//import java.util.concurrent.Executor;
-//import java.util.concurrent.Executors;
-//
-//@Service
-//public class MechanicsExecutor implements Runnable {
-//    private static final Logger LOGGER = LoggerFactory.getLogger(MechanicsExecutor.class);
-//
-//    private final Executor tickExecutor = Executors.newSingleThreadExecutor();
-//
-//    public MechanicsExecutor() {
-//
-//    }
-//
-//    @PostConstruct
-//    public void initAfterStartup() {
-//        tickExecutor.execute(this);
-//    }
-//
-//    @Override
-//    public void run() {
-//        gameLoop();
-//    }
-//
-//    private void gameLoop() {
-//
-//    }
-//}
+package ru.mail.park.mechanics;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import static ru.mail.park.info.constants.Constants.MICRO_SECOND;
+import static ru.mail.park.info.constants.Constants.TICK;
+
+@Service
+public class MechanicsExecutor implements Runnable {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MechanicsExecutor.class);
+
+    private final Executor tickExecutor = Executors.newSingleThreadExecutor();
+
+    public MechanicsExecutor() {
+
+    }
+
+    @PostConstruct
+    public void initAfterStartup() {
+        tickExecutor.execute(this);
+    }
+
+    @Override
+    public void run() {
+        gameLoop();
+    }
+
+    private void gameLoop() {
+        long beforeTime = System.nanoTime();
+        long afterTime;
+        long sleepTime;
+
+        while (true) {
+
+
+            afterTime = System.nanoTime();
+            sleepTime = Math.max(0, TICK - (afterTime - beforeTime) / MICRO_SECOND);
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                LOGGER.warn("Thread is interrupted");
+            }
+        }
+    }
+}
