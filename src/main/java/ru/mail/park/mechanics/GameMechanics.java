24707ba65aeb11ceee51e2d4ae451edb78c9ed9d
ArturBmstu <artur050711@gmail.com>
2017-11-29 21:51:35 +0400

    mechanics refactoring started

--- src/main/java/ru/mail/park/mechanics/GameMechanics.java
+++ src/main/java/ru/mail/park/mechanics/GameMechanics.java
@@ -14,11 +14,10 @@ import ru.mail.park.services.UserDao;
 import ru.mail.park.websocket.message.to.FinishedMessage;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 @Service
 public class GameMechanics {
@@ -28,8 +27,8 @@ public class GameMechanics {
     private final GameSessionService gameSessionService;
     private static final Logger LOGGER = LoggerFactory.getLogger(GameMechanics.class);
 
+    private Map<Id<Board>, Set<Id<User>>> boardUserMap = new ConcurrentHashMap<>();
     private Map<Id<User>, Id<Board>> userBoardMap = new ConcurrentHashMap<>();
-    private Queue<Id<User>> waiters = new ConcurrentLinkedQueue<>();
     private Queue<Runnable> tasks = new ConcurrentLinkedQueue<>();
 
     public GameMechanics(
@@ -46,24 +45,75 @@ public class GameMechanics {
 
     public void addWaiter(Id<User> userId, Id<Board> board) {
         if (gameSessionService.isPlaying(userId)) {
+            LOGGER.warn("Player is in game now");
             return;
         }
-        waiters.add(userId);
+        Id<Board> found = userBoardMap.get(userId);
+        if (found != null) {
+            if (found == board) {
+                LOGGER.warn("Already subscribed on this board");
+                return;
+            } else {
+                LOGGER.warn("Resubscribing on another board");
+
+            }
+        }
+        if (gameDao.getMetaOf(board.getId()) == null) {
+            LOGGER.error("Subscribed on bad board. Closing session");
+            remotePointService.cutDownConnection(userId, CloseStatus.SERVER_ERROR);
+            return;
+        }
+        boardUserMap.putIfAbsent(board, new ArrayList<>());
+        boardUserMap.get(board)
+                .add(userId);
         userBoardMap.put(userId, board);
         LOGGER.info("User added in queue");
     }
 
-    public void tryStartGame(Id<User> userId, Id<Board> boardId) {
+    public List<Id<User>> matchPlayers(Set<Id<User>> waiters, int players) {
+        int i = 0;
+        List<Id<User>> matchedPlayers = new ArrayList<>();
+        Iterator<Id<User>> waitersIterator = waiters.iterator();
+        while (i < players && waitersIterator.hasNext()) {
+            Id<User> waiter = waitersIterator.next();
+            if (!checkCandidate(waiter)) {
+                waitersIterator.remove();
+                continue;
+            }
+            matchedPlayers.add(waiter);
+            i++;
+        }
+        if (i < players) {
+            return null;
+        }
+        waiters.removeAll(matchedPlayers);
+        return matchedPlayers;
+    }
+
+    public void tryStartGame() {
         LOGGER.warn("Trying to start the game");
+
+        boardUserMap.forEach((boardId, waiters) -> {
+            BoardMeta meta = gameDao.getMetaOf(boardId.getId());
+            int players = meta.getPlayers();
+            List<Id<User>> matchedPlayers;
+            while (waiters.size() % players > 0) {
+                matchedPlayers = matchPlayers(waiters, players);
+                if (matchedPlayers != null) {
+                    gameSessionService.startGame(boardId, matchedPlayers);
+                }
+            }
+        });
+
         if (gameSessionService.isPlaying(userId)) {
             LOGGER.warn("Player is in game now");
             return;
         }
-        userBoardMap.remove(userId);
+        boardUserMap.remove(userId);
         if (!checkCandidate(userId)) {
             return;
         }
-        BoardMeta meta = gameDao.getMetaOf(boardId.getId());
+
         int players = 1;
         if (meta != null) {
             players = meta.getPlayers();
@@ -77,12 +127,12 @@ public class GameMechanics {
             gameSessionService.startGame(userId, null, boardId);
             return;
         }
-        LOGGER.info("Map size: " + userBoardMap.size());
-        for (Map.Entry<Id<User>, Id<Board>> entry : userBoardMap.entrySet()) {
+        LOGGER.info("Map size: " + boardUserMap.size());
+        for (Map.Entry<Id<User>, Id<Board>> entry : boardUserMap.entrySet()) {
             if (entry.getValue().equals(boardId) && !entry.getKey().equals(userId)) {
                 Id<User> opponent = entry.getKey();
                 if (!checkCandidate(opponent)) {
-                    userBoardMap.remove(opponent);
+                    boardUserMap.remove(opponent);
                     LOGGER.warn("Opponent is not connected or playing or he doesn't exists");
                     continue;
                 }
@@ -93,7 +143,7 @@ public class GameMechanics {
                 return;
             }
         }
-        userBoardMap.put(userId, boardId);
+        boardUserMap.put(userId, boardId);
         LOGGER.info("Player added!");
     }
 
@@ -138,6 +188,6 @@ public class GameMechanics {
         LOGGER.warn("Removing board waiter with username %s",
                 userDao.findUserById(userId.getId()).getUsername()
         );
-        userBoardMap.remove(userId);
+        boardUserMap.remove(userId);
     }
 }
