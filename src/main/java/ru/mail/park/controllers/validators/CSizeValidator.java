ebf0248ae8941cbe1aff7bc0c7b96219e789b6fd
ArturBmstu <artur050711@gmail.com>
2017-12-22 01:51:57 +0300

    deprecated erunda fix

--- src/main/java/ru/mail/park/controllers/validators/CSizeValidator.java
+++ src/main/java/ru/mail/park/controllers/validators/CSizeValidator.java
@@ -1,15 +1,11 @@
 package ru.mail.park.controllers.validators;
 
-import org.hibernate.validator.internal.util.logging.Log;
-import org.hibernate.validator.internal.util.logging.LoggerFactory;
 import ru.mail.park.controllers.validators.constraints.CSize;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class CSizeValidator implements ConstraintValidator<CSize, CharSequence> {
-    private static final Log LOG = LoggerFactory.make();
-
     private int min;
     private int max;
 
@@ -17,7 +13,6 @@ public class CSizeValidator implements ConstraintValidator<CSize, CharSequence> 
     public void initialize(CSize parameters) {
         min = parameters.min();
         max = parameters.max();
-        validateParameters();
     }
 
     @Override
@@ -27,17 +22,5 @@ public class CSizeValidator implements ConstraintValidator<CSize, CharSequence> 
         }
         final int length = charSequence.length();
         return length >= min && length <= max;
-    }
-
-    private void validateParameters() {
-        if (min < 0) {
-            throw LOG.getMinCannotBeNegativeException();
-        }
-        if (max < 0) {
-            throw LOG.getMaxCannotBeNegativeException();
-        }
-        if (max < min) {
-            throw LOG.getLengthCannotBeNegativeException();
-        }
     }
 }
